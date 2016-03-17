7c8e659673f58a9238dd7ae10079c9008e9e70d8
patrick vares <pvrs12@gmail.com>
2016-03-17 04:10:43 +0000

    added nerdcommenter

--- install-plugins.bash
+++ install-plugins.bash
@@ -10,3 +10,4 @@ git clone https://github.com/vim-airline/vim-airline --recursive --depth=1
 git clone https://github.com/vim-airline/vim-airline-themes --recursive --depth=1
 git clone https://github.com/tpope/vim-fugitive --recursive --depth=1
 git clone https://github.com/airblade/vim-gitgutter --recursive --depth=1
+git clone https://github.com/scrooloose/nerdcommenter --recursive --depth=1
