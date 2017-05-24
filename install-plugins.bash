bbafc3fb3a024c041d27e15929cbc9bdf26e662e
Patrick Vares <p.vares@elsevier.com>
2017-05-24 14:03:54 -0400

    added multiple file sources for installing plugins

--- install-plugins.bash
+++ install-plugins.bash
@@ -3,6 +3,10 @@ VIM_DIR=~/.vim
 VIM_PATHOGEN_DIR=$VIM_DIR/autoload
 VIM_PLUGIN_DIR=$VIM_DIR/bundle
 
+BASE_DIR=$(pwd)
+
+PLUGIN_DIR=pluginlist.d
+
 cp .vimrc ~/.vimrc
 cp .vimrcfunc ~/.vimrcfunc
 
@@ -16,13 +20,19 @@ mkdir -p $VIM_PATHOGEN_DIR
 mkdir -p $VIM_PLUGIN_DIR
 cp pathogen.vim $VIM_PATHOGEN_DIR/pathogen.vim
 
-while read l; do
-	l=${l##*( )}
-	if [[ $l == \#* ]]; then
-		continue
-	fi
-	if [[ $l == '' ]]; then
-		continue
-	fi
-	get_plugin $l
-done < pluginlist
+for f in $PLUGIN_DIR/*; do
+	f=$BASE_DIR/$f
+	echo ""
+	echo "Installing plugins from '$f'..."
+
+	while read l; do
+		l=${l##*( )}
+		if [[ $l == \#* ]]; then
+			continue
+		fi
+		if [[ $l == '' ]]; then
+			continue
+		fi
+		get_plugin $l
+	done < $f
+done
